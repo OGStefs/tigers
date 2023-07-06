6fd537110d3903e01ed06c5227f5c5edcdb36f5a
OGStefs <stefanistweg@gmail.com>
2023-07-06 06:38:42 +0200

    working

--- resources/scheduler.js
+++ resources/scheduler.js
@@ -1,46 +1,57 @@
 import axios from "axios";
 import dotenv from "dotenv";
+import { safeToFile } from "./safeToFile.js";
 
 dotenv.config();
 
-const wallet = "0xe7D7C015aF94b282859184CABed04fF23a24bB1B";
+const list = {};
+let cursor = "";
+let works = true;
+const limit = `&limit=${100}`;
+const url = "https://deep-index.moralis.io/api/v2/nft/";
+const contract = "0xd124d5200c95648c9d1eb958a51e610b2a395351";
+const appendix = `/owners?chain=eth&format=decimal${limit}&media_items=false&cursor=`;
 
-const getPasses = async () => {
-  const tokensOwned = [];
-  const options = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "X-API-Key": process.env.MORALIS,
-    },
-  };
-
-  //   for (let i = 0; i < keys.length; i++) {
-  try {
-    const response = await axios(
-      `https://deep-index.moralis.io/api/v2/${wallet}/nft?chain=eth&format=decimal&token_addresses%5B0%5D=0xd124d5200c95648c9d1eb958a51e610b2a395351&media_items=false`,
-      options
-    ).catch(async (err) => {
-      //   await sendEmail(
-      //     "key moralis error",
-      //     `${err.response.status}, ${err.response.data}`
-      //   );
-      console.error(
-        "error from axios in getKeyOwnersExternal moralis call",
-        err.response.status,
-        err.response.data
-      );
-    });
-
-    console.log(response.data.result.length);
-    response?.data?.result.forEach((token) => {
-      tokensOwned.push(token.token_id);
-    });
-
-    console.log(tokensOwned.sort((a, b) => a - b));
-  } catch (error) {
-    console.error("error from try/catc in getKeyOwnersExternal", error.message);
-  }
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "X-API-Key": process.env.MORALIS,
+  },
 };
 
-await getPasses();
+while (works) {
+  const test = await axios(url + contract + appendix + cursor, options).catch(
+    (error) => {
+      console.log("error here");
+      console.log(error.message);
+      works = false;
+    }
+  );
+
+  // break loop when status is not ok
+  if (!test?.statusText === "OK") works = false;
+
+  //   console.log("Status", test?.statusText);
+  //   console.log(test?.data.page);
+  //   console.log(test.data.cursor);
+
+  test?.data?.result?.forEach((res) => {
+    if (list.hasOwnProperty(res.owner_of)) {
+      list[res.owner_of].push(parseInt(res.token_id));
+    } else {
+      list[res.owner_of] = [parseInt(res.token_id)];
+    }
+  });
+  if (test?.data?.cursor) {
+    cursor = test.data.cursor;
+  } else works = false;
+}
+
+// sort items in owner array
+for (const [key, value] of Object.entries(list)) {
+  list.key = value.sort((a, b) => a - b);
+}
+console.log(list);
+
+await safeToFile(list, "vpass_owners");
